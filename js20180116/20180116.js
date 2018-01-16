34ef739d47b7744ba31a4603d4edd7c69da882b9
anciuvis <nonofthem@gmail.com>
2018-01-16 10:45:27 +0200

    corr IF to bulletproof myVar null value

--- js20180116/20180116.js
+++ js20180116/20180116.js
@@ -7,7 +7,7 @@ console.log(p);
 // document.write - prideda i gala teksta, todel sunkiau valdyti kur jis atsidurs, vengti naudoti realiai
 // setTimeout- jeigu noriu kad pradetu vykdyti funkcija po kazkiek laiko, pvz 200ms delay. su salyga kad pele buvouzvaziuota ant paveikslelio pvz.
 // funkcija setTimeout grazina
-var myVar;
+var myVar = null;
 function mouseIn() {
 	document.getElementById('status').innerHTML = 'pele uzvaziavo';
 	myVar = setTimeout(mouseSleep, 500);
@@ -15,9 +15,12 @@ function mouseIn() {
 
 function mouseSleep() {
 	document.getElementById('status').innerHTML = 'pele uzmigo';
+	myVar = null;
 }
 
 function mouseOut() {
 	document.getElementById('status').innerHTML = 'pele isvaziavo';
+	if (myVar) {
 	clearTimeout(myVar);
+	}
 }
