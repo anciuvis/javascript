a64f1351e8ed46a36cad935debf76112ded778ee
anciuvis <nonofthem@gmail.com>
2018-01-16 10:38:56 +0200

    mouseIn-Out function

---
+++ js20180116/20180116.js
@@ -0,0 +1,23 @@
+// innerHTML - iraso i tago viduri teksta, kuris bus skaitomas kaip HTML kodas, tai realiai galima viduj prikurti puslapio
+let d = document.getElementById('mdiv');
+console.log(d.innerHTML);
+d.innerHTML = 'blabla <p id="pid">bla</p>!';
+let p = document.getElementById('pid');
+console.log(p);
+// document.write - prideda i gala teksta, todel sunkiau valdyti kur jis atsidurs, vengti naudoti realiai
+// setTimeout- jeigu noriu kad pradetu vykdyti funkcija po kazkiek laiko, pvz 200ms delay. su salyga kad pele buvouzvaziuota ant paveikslelio pvz.
+// funkcija setTimeout grazina
+var myVar;
+function mouseIn() {
+	document.getElementById('status').innerHTML = 'pele uzvaziavo';
+	myVar = setTimeout(mouseSleep, 500);
+}
+
+function mouseSleep() {
+	document.getElementById('status').innerHTML = 'pele uzmigo';
+}
+
+function mouseOut() {
+	document.getElementById('status').innerHTML = 'pele isvaziavo';
+	clearTimeout(myVar);
+}
