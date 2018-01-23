955ec8c802adbe43c8500b28e808056cf32eda2a
anciuvis <nonofthem@gmail.com>
2018-01-23 16:42:02 +0200

    upd

--- js20180116/20180116-2.js
+++ js20180116/20180116-2.js
@@ -25,94 +25,188 @@ function validateForm() {
 	// var about = document.forms["myForm"]["about"].value;
 	// console.log(about);
 
+	// 	if (user == ""||pass == ""||name == ""||country == ""||email == ""||sex == "") {
+	// 		alert(notAllow);
+	// 		return false;
+	// 	}
+	// 	console.log('gerai');
+	// }
+
 	// USERNAME VALIDATION
 	if (user.length<5||user.length>12) {
 		alert('username length not right');
-	}
-	let found = false;
-	for (let i = 0; i < user.length; i++) {
-		console.log(user[i]);
-		if (user[i]>='0'&&user[i]<='9') {
-			found = true;
-			break;
-		}
-	}
-	if (!found) {
-		console.log('klaida');
-		alert('username must contain number');
+		console.log('username ilgis blogas');
 		return;
 	}
+	console.log('username ilgis geras');
+
+	let rLett = /[A-Za-z]/;
+	let rDig = /\d/;
+	let rSpec = /[!.,;?[\]{}/\\*(-)-+_#@&$|'\"><^`~=]/;
+
+	if (rLett.test(user)) {
+		console.log('raides username yra');
+	} else {
+		alert('username must contain letters');
+		console.log('raidziu username nera');
+		return;
+	}
+
+	if (rDig.test(user)) {
+		console.log('skaicius username yra');
+	} else {
+		alert('username must contain number');
+		console.log('skaiciaus username nera');
+		return;
+	}
+
+	if (rSpec.test(user)) {
+		alert('username cannot contain special symbols');
+		console.log('username yra special symbols');
+		return;
+	} else {
+		console.log('username nera spec.simboliu');
+	}
+	// senas variantas tikrinimui ar yra skaicius (be Reg Expo)
+	// let found = false;
+	// for (let i = 0; i < user.length; i++) {
+	// 	console.log(user[i]);
+	// 	if (user[i]>='0'&&user[i]<='9') {
+	// 		found = true;
+	// 		break;
+	// 	}
+	// }
+	// if (!found) {
+	// 	console.log('klaida');
+	// 	alert('username must contain number');
+	// 	return;
+	// }
+	// let r = /\d/; // situo atveju galima rasyti ir \d+ ir \d tiesiog
+	// 	if (r.test(user)) {
+	// 		console.log('skaicius yra');
+	// 	} else {
+	// 		console.log('skaiciaus nera');
+	// 		alert('username must contain number');
+	// 		return;
+	// 	}
+	// }
+
 
 	// PASSWORD VALIDATION
 	if (pass.length<7||pass.length>12) {
 		alert('password length not right');
+		console.log('passo ilgis blogas');
+		return;
 	}
-	found = false;
-	for (let i = 0; i < pass.length; i++) {
-		console.log(pass[i]);
-		if (pass[i]>='0'&&pass[i]<='9') {
-			found = true;
-			break;
-		}
+	console.log('passo ilgis geras');
+
+	if (rLett.test(pass)) {
+		console.log('raides passworde yra');
+	} else {
+		alert('password must contain letters');
+		console.log('raidziu passworde nera');
+		return;
 	}
-	if (!found) {
-		console.log('klaida');
+
+	if (rDig.test(pass)) {
+		console.log('skaicius passworde yra');
+	} else {
 		alert('password must contain number');
+		console.log('skaiciaus passworde nera');
 		return;
 	}
-	found = false;
-	for (let i = 0; i < pass.length; i++) {
-		console.log(pass[i]);
-		const spc = ["!", ".", ",", ";", "?", "[", "]", "{", "}", "/", "\\", "*", "(", ")", "-", "+", "_", "#", "@", "&", "$", "|", "'", "\"", ">", "<", "^", "`", "~", "="]
-		// kad neaprasineti ilgos salygos kaip apacioj, padarom masyva is spec.simboliu
-		// if (pass[i] === "!"||pass[i] === "."||pass[i] === ","||pass[i] === ";"||pass[i] === "?"||pass[i] === "["||pass[i] === "]"||pass[i] === "{"||pass[i] === "}"||pass[i] === "/"||pass[i] === "\\"||pass[i] === "*") {
-		if (spc.indexOf(pass[i])>=0) {
-			found = true;
-			break;
-		}
-	}
-	if (!found) {
-		console.log('klaida');
+
+	if (rSpec.test(pass)) {
+		console.log('spec.simbolis passworde yra');
+	} else {
 		alert('password must contain special symbol');
+		console.log('spec.simboliaus passworde nera');
 		return;
 	}
+
 }
-
-//RegExp
-let r1 = new RegExp ('\d+'); // tas pats kaip r2, retai taip uzrasoma
-let r2 = /\d+/;
-
-let r3 = new RegExp ('[a-q]*','i'); // tas pats kaip r4
-let r4 = /[a-q]*/i;
-
-let s = 'ABC3DGc';
-r2 = /\d/; // situo atveju galima rasyti ir \d+ ir \d tiesiog
-if (r2.test(s)) {
-	console.log('skaicius yra');
-} else {
-	console.log('skaiciaus nera');
-}
-
-r2 = /^[aA].*[cC]$/; // kad prasideda a arba A ir baigiasi c arba C, tarp ju nesvarbu skaicius, .* reiskia kad salyga gali kartotis arba nuli arba n kartu (t.y. neprivaloma kad kartotusi isvis)
-// r2 = /^[a].*[c]$/i; - tas pats, i pasako kad tinka mazosios ir didziosios
-if (r2.test(s)) {
-	console.log('salyga yra');
-} else {
-	console.log('salygos nera');
-}
-
-s = 'apple is red and tasty';
-console.log(s);
-r2 = /(red)|(yellow)/; // surasti kad yra 'red' arba 'yellow'
-if (r2.test(s)) {
-	console.log('red/yellow yra');
-} else {
-	console.log('red/yellow nera');
-}
-
-// 	if (user == ""||pass == ""||name == ""||country == ""||email == ""||sex == "") {
-// 		alert(notAllow);
-// 		return false;
+		// if (pass[i] === "!"||pass[i] === "."||pass[i] === ","||pass[i] === ";"||pass[i] === "?"||pass[i] === "["||pass[i] === "]"||pass[i] === "{"||pass[i] === "}"||pass[i] === "/"||pass[i] === "\\"||pass[i] === "*") {
+		// kad neaprasineti ilgos salygos kaip apacioj, padarom masyva is spec.simboliu
+// 		const spc = ["!", ".", ",", ";", "?", "[", "]", "{", "}", "/", "\\", "*", "(", ")", "-", "+", "_", "#", "@", "&", "$", "|", "'", "\"", ">", "<", "^", "`", "~", "="]
+// 		if (spc.indexOf(pass[i])>=0) {
+// 			found = true;
+// 			break;
+// 		}
 // 	}
-// 	console.log('gerai');
+// 	if (!found) {
+// 		console.log('klaida');
+// 		alert('password must contain special symbol');
+// 		return;
+// 	}
 // }
+
+// // RegExp bandymai
+// let r1 = new RegExp ('\d+'); // tas pats kaip r2, retai taip uzrasoma
+// let r2 = /\d+/;
+//
+// let r3 = new RegExp ('[a-q]*','i'); // tas pats kaip r4
+// let r4 = /[a-q]*/i;
+//
+// let s = 'ABC3DGc';
+// r2 = /\d/; // situo atveju galima rasyti ir \d+ ir \d tiesiog
+// if (r2.test(s)) {
+// 	console.log('skaicius yra');
+// } else {
+// 	console.log('skaiciaus nera');
+// }
+//
+// r2 = /^[aA].*[cC]$/; // kad prasideda a arba A ir baigiasi c arba C, tarp ju nesvarbu skaicius, .* reiskia kad salyga gali kartotis arba nuli arba n kartu (t.y. neprivaloma kad kartotusi isvis)
+// // r2 = /^[a].*[c]$/i; - tas pats, i pasako kad tinka mazosios ir didziosios
+// if (r2.test(s)) {
+// 	console.log('salyga yra');
+// } else {
+// 	console.log('salygos nera');
+// }
+//
+// s = 'apple is rĄed and tastąy';
+// console.log(s);
+// r2 = /(red)|(yellow)/; // surasti kad yra 'red' arba 'yellow'
+// r3 = /\u0104/;
+// if (r2.test(s)) {
+// 	console.log('red/yellow yra');
+// } else {
+// 	console.log('red/yellow nera');
+// }
+//
+// r3 = /\u0104/;
+// if (r3.test(s)) {
+// 	console.log('Ą yra');
+// } else {
+// 	console.log('Ą nera');
+// }
+// if (s.search(r2)>=0) { // s.search (r2) grazina match'o eilini numeri jeigu ji rado. arba -1 jeigu nerado
+// 	console.log('Ą yra');
+// } else {
+// 	console.log('Ą nera');
+// }
+//
+// // s.match(r2);
+// // r2.exec(s); // grazina masyva
+//
+// s = 'apple is red and tasty';
+// r2 = /(red).*(ta)/i; // surasti
+// if (s.search(r2)>=0) {
+// 	console.log(s.match(r2));
+// 	console.log('red ir/ar ta yra');
+// } else {
+// 	console.log('red ir/ar ta nera');
+// }
+// s = s.replace(r2,"$2$1");
+// console.log(s);
+//
+// //apkeicia vietomis du zodzius
+// s = 'red tasty';
+// r2 = /(\w*) (\w*)/i;
+// if (s.search(r2)>=0) {
+// 	console.log(s.match(r2));
+// 	console.log('yra');
+// } else {
+// 	console.log('nera');
+// }
+// s = s.replace(r2,"$2 $1");
+// console.log(s);
