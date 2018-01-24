a878b393dfec89f7200840a31164dc5156749c39
anciuvis <nonofthem@gmail.com>
2018-01-24 13:48:06 +0200

    fhytg

--- js20180124/script.js
+++ js20180124/script.js
@@ -21,18 +21,64 @@
 
 // tam kad keisti klases tam tikrai grupei elementu in pure JS reikia tokio kodo:
 // gali buti classList.add / classList.toggle, priklausomai nuo to ko reikia
-function testas () {
-	let list = document.getElementsByTagName('label');
-	for (var i = 0; i < list.length; i++) {
-		list[i].classList.toggle("red");
-	}
-	console.log('suveikia class change');
-}
+// reikia prirasyti paciam HTMLE i tag'a: onclick (testas();)
+// function testas () {
+// 	let list = document.getElementsByTagName('label');
+// 	for (var i = 0; i < list.length; i++) {
+// 		list[i].classList.toggle("red");
+// 	}
+// 	console.log('suveikia class change');
+// }
+// ira toks pat su hiddem  klase:
+// function show () {
+// 	let list = document.getElementsByTagName('form');
+// 	for (var i = 0; i < list.length; i++) {
+// 		list[i].classList.toggle("hidden");
+// 	}
+// 	console.log('suveikia class change');
+// }
+// ta pati su jQuery paprasciau:
+$(function() {
+	$('#btn-new').click(function() {
+		$('form').toggleClass('hidden');
+	});
+});
+// $('div');
+// $('.manoklase');
+// $(#manoId);
+// - visi sitie variantas duoda rezultata SARASA
+// $('<div>'); - sukuria nauja div elementa
+// $('<div><p>blabla</p></div>'); - bei sudetingesnius dalykus irgi gali, skirtingus HTML objektus
+// bet dar nera niekam priskirta - kaba DOMe siaip
+// reikia kazkam priskirti pagal analogija kaip DOMo appendChild() metodas pvz:
+// var d = $('<div><p>blabla</p></div>');
+// $('#mId').append(d);
+// $('#mId').append($('<div>'));
+//
+// $('#mygt').mouseover(){ - ant konkretaus elemento eventas
+//	...
+// }
+//
+// $('.mygt').mouseover(){ - ant visu klasiu mygtuku
+//	...
+// }
 
-function show () {
-	let list = document.getElementsByTagName('form');
-	for (var i = 0; i < list.length; i++) {
-		list[i].classList.toggle("hidden");
-	}
-	console.log('suveikia class change');
-}
+
+$( document ).ready(function() {
+	alert( "welcome" );
+	$( "a" ).click(function( event ) {
+		alert( "As you can see, the link no longer took you to jquery.com" );
+		event.preventDefault();
+	});
+	$( "#btn-list" ).click(function () {
+		alert( "veikia" );
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("listLoad").innerHTML = this.responseText;
+			}
+		};
+		xhttp.open("GET", "http://192.168.1.81:8080/list", true);
+		xhttp.send();
+	});
+});
